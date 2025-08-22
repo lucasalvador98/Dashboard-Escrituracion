f653b832c9adcf764ba73fed5e1489f7e48ca8ea
Dir-Tecno <tecnologiahabitatyfamilia@gmail.com>
2025-08-22 12:26:37 -0300

    Update config-api.js

--- frontend/src/config-api.js
+++ frontend/src/config-api.js
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_URL || 'http://172.20.0.11:8000',
+  BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
 };
 
 export default API_CONFIG;
